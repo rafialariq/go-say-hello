7812a2abe43f6259793536459185e4997c16b6c3
Rafi Alariq <rmalariq@gmail.com>
2022-11-21 16:21:10 +0700

    update ke versi 1.7.0

--- say_hello.go
+++ say_hello.go
@@ -3,6 +3,6 @@ package go_say_hello
 import "fmt"
 
 func SayHello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome and congratulation!", name)
 	return message
 }
